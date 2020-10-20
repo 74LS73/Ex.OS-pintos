e838c5a3a6a269e01671917b19749cd8768185af
Lighten-w <664185376@qq.com>
2020-10-20 20:35:56 +0800

    project1-2 test2

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -136,6 +136,7 @@ void thread_foreach (thread_action_func *, void *);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
+bool compare_thread_priority (const struct list_elem *a, const struct list_elem *b, void *aux);
 
 int thread_get_nice (void);
 void thread_set_nice (int);
